e64841761a9cd7f38784e52e86b77764dc8ac0da
Vincente <vinmanneal@gmail.com>
2025-08-05 20:10:28 -0500

    MAde the camera rotate with player mouse input. and added cursor lock

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float speed = 10f;
     private Vector3 movement;
     private Rigidbody rb;
+    public float rotationSpeed = 5.0f;
+    public string cameraRotateAxis;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -18,12 +23,19 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
 
         Move(horizontalInput, verticalInput);
+        RotateCamera();
     }
 
     private void Move(float horizontalInput, float verticalInput)
     {
-        movement.Set (horizontalInput, 0f, verticalInput);
+        movement.Set(horizontalInput, 0f, verticalInput);
         movement = movement.normalized * speed * Time.deltaTime;
         rb.MovePosition(transform.position + movement);
+    }
+
+    private void RotateCamera()
+    {
+        float rotationInput = Input.GetAxis(cameraRotateAxis);
+        transform.Rotate(Vector3.up, rotationInput * rotationSpeed * Time.deltaTime);
     }
 }
